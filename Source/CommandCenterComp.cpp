c3fa81de8cd5009e5c287fe0dd169ce66d28010c
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2025-04-15 10:38:32 +0900

    오류수정

--- Source/CommandCenterComp.cpp
+++ Source/CommandCenterComp.cpp
@@ -127,7 +127,7 @@ void CommandCenterComp::update(float delta)
             {
                 mTimer = 0.f;
                 DeleteSCV();
-                if (IsUnitArrayEmpty)
+                if (IsUnitArrayEmpty())
                 {
 
                     IsCreatingUnit = false;
