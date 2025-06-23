c51b6789ce2f7f414f48462e5c114c1dc96aa55e
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2025-06-23 10:41:59 +0900

    건설할때 이펙트 추가

--- Source/Goal/Action/Goal_WaitTime.cpp
+++ Source/Goal/Action/Goal_WaitTime.cpp
@@ -6,6 +6,7 @@
 #include "Goal/Base/Goal.h"
 #include "Goal/Base/Goal_Think.h"
 #include "Goal_WaitTime.h"
+#include "../../DrawComp.h"
 
 const char* Goal_WaitTime::GOAL_NAME = "Goal_WaitTime";
 Goal_WaitTime::Goal_WaitTime(Actor* actor)
@@ -23,6 +24,8 @@ void Goal_WaitTime::Start()
     mActor->mGoalComp->mCurGoal = GoalType::WaitTime;
 
     mActor->mMoveComp->Start_ElapsedTime();
+    auto node = mActor->mDrawComp->CreateGatheringNode(mActor->mActorType);
+    node->setPosition(mActor->mDrawComp->GetCarryItemPos(mActor->mDrawComp->mCurDir));
 }
 
 int Goal_WaitTime::Do()
@@ -33,6 +36,8 @@ int Goal_WaitTime::Do()
     {
         m_Status = Goal::completed_t;
         mActor->mUnitComp->mCurAction = ActionState::Idle;
+        auto gatherNode               = mActor->GetRoot()->getChildByName("GatherAnim");
+        gatherNode->removeFromParentAndCleanup(true);
         return m_Status;
     }
 
